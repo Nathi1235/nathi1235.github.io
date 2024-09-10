1e5bf06635a59e8b59810518c20519e0960cfd2f
Nathi1235 <73069439+Nathi1235@users.noreply.github.com>
2024-09-10 14:33:57 +0200

    Update script.js

--- Disease_Guesser/script.js
+++ Disease_Guesser/script.js
@@ -43,5 +43,5 @@ function check(){
 function reveal(){
     document.getElementById("disease").innerHTML = "<p style='color:blue'>"+disease+"</p>";
     document.getElementById("measures").innerHTML = "Maßnahmen:"+measures;
-    document.getElementById("cABCDE").innerHTML = "<br><br>"+JSON.stringigy(diseases[disease]);
+    document.getElementById("cABCDE").innerHTML = "<br><br>"+JSON.stringigy(data[disease]);
 }
