6e300e08f97838a3129a603d4a32859ed599c29c
Nathi1235 <eiternathanael@gmail.com>
2025-06-05 18:26:00 +0200

    add standard to industry time conversion

--- IndustryTimeConverter/script.js
+++ IndustryTimeConverter/script.js
@@ -1,7 +1,33 @@
 function conv(){
-    let input = document.getElementById("input").value;
-    input = input.replace(/[^0-9|+*/.-]+/g, ''); // remove all whitespace
-    let result = eval(input);
-    let output = "Industry Time: " + result + "h<br>Standard Time: " + parseInt(result) + "h" + parseInt(result%1 * 60);
-    document.getElementById("result").innerHTML = output;
+    var input = document.getElementById("input").value;
+    console.log("Input: " + input);
+    input = input.replace(/[^0-9|+*/.,h-]+/g, ''); // remove all whitespace
+    console.log("Filter Input: " + input);
+    let a = input.split(/[+*/-]+/);
+    console.log("Filter Numbers: " + a.join("; "));
+    try{
+    for (let i = 0; i < a.length; i++) {
+        if (a[i].includes('h') || a[i].includes(',')) {
+            console.log("Filter Standard Time Numbers: " + a[i]);
+            let b = a[i].split(/[h,]+/);
+            let c = parseFloat(b[0]) + (b[1] ? parseFloat(b[1]) / 60 : 0);
+            console.log("Convert to Industry Time: " + c);
+            input = input.replace(a[i].toString(), c);
+            console.log("Replace in original String: " + input);
+        }
+    }
+    } catch (e) {
+        console.error("Error in conversion: " + e);
+        document.getElementById("result").innerHTML = "Invalid input. Please check your expression.";
+        return;
+    }
+    try{
+        let result = eval(input);
+        console.log("Result: " + result);
+        let output = "Industry Time: " + result + "h<br>Standard Time: " + parseInt(result) + "h" + parseInt(result%1 * 60);
+        document.getElementById("result").innerHTML = output;
+    } catch (e) {
+        console.error("Error in evaluation: " + e);
+        document.getElementById("result").innerHTML = "Invalid input. Please check your expression.";
+    }
 }
